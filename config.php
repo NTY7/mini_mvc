fee62ab3075327f66c4845123c09342f8c2570ba
nty <yannickntonga@icloud.com>
2025-02-18 17:02:54 +0100

    Ntonga yaniick

--- config.php
+++ config.php
@@ -3,3 +3,4 @@ define("_BOOKS_IMAGES_FOLDER_", "/uploads/books/");
 define("_ASSETS_IMAGES_FOLDER_", "/assets/images/");
 define("_HOME_BOOK_LIMIT_", 3);
 define("_ITEM_PER_PAGE_", 9);
+define('_ASSETS_PATH_', '/mini_mvc/assets');
